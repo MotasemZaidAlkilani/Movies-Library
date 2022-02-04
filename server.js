db93047a1588bb205016d6d9d50c68c8fdc13f41
Mot'asem Zaid alkilani <72309669+MotasemZaidAlkilani@users.noreply.github.com>
2022-02-04 21:07:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const cors=require('cors');
 const axios=require('axios');
 const data=require('./Movie Data/data.json');
 
-const client = new pg.Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+const client = new pg.Client(
+    process.env.DATABASE_URL);
+   
 const server=express();
 server.use(cors());
 server.use(express.json());
